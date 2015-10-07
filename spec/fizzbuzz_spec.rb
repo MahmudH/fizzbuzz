c2ee81b32f6dfec6fc0df1537f30d42802492e59
mahmudh <mahmudhssn246@gmail.com>
2015-10-07 13:23:31 +0100

    added a test to check if fizz is returned when given 4

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
+require 'fizzbuzz'
 #Test 1 - return fizz if div by 3.
 
 describe 'fizzbuzz' do
   it "it returns fizz when given 3" do
     expect(fizzbuzz(3)).to eq("fizz")
   end
+
+  it "it does not return fizz when given 4" do
+  	expect(fizzbuzz(4)).not_to eq('fizz')
+  end
+
 end
